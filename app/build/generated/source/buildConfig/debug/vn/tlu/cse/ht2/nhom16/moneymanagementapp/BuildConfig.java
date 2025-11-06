474e14de610a5d1f45f064a0562ff2184d69eeb6
Phong Nguyen <phongnhp18@gmail.com>
2025-11-06 23:13:24 +0700

    Save current work before merging master

---
+++ app/build/generated/source/buildConfig/debug/vn/tlu/cse/ht2/nhom16/moneymanagementapp/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package vn.tlu.cse.ht2.nhom16.moneymanagementapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "vn.tlu.cse.ht2.nhom16.moneymanagementapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String GEMINI_API_KEY = "AIzaSyCZTFp9SaPBIyu5lUbbv9hKjpFne2bzmxA";
+}
